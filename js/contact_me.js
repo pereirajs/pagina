0de49f993a0041466c8a5a11c368fe806db4c399
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-03-20 11:26:18 -0500

    Hack temporal para los mensajes directos

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
+            /*$.ajax({
                 url: "https://webhooks.gitter.im/e/e405119b166282e1f6e5",
                 type: "POST",
                 data: "message="+encodeURIComponent("##"+name+"\n###"+email+"\n####"+twitter+"\n\n"+message),
@@ -46,7 +46,20 @@ $(function() {
                     //clear all fields
                     //$('#contactForm').trigger("reset");
                 }
-            })
+            })*/
+            $.post( "https://webhooks.gitter.im/e/e405119b166282e1f6e5", {message : "##"+name+"\n#### "+email+"\n#### "+twitter+"\n\n"+message}).always(function(data) {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-success')
+                    .append("<strong>Your message has been sent. </strong>");
+                $('#success > .alert-success')
+                    .append('</div>');
+
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            });
         },
         filter: function() {
             return $(this).is(":visible");

f5168eb7d505e110beb1335d5ee84cdc1e86a3ad
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-03-20 11:44:32 -0500

    Mensaje en español

--- js/contact_me.js
+++ js/contact_me.js
@@ -53,7 +53,7 @@ $(function() {
                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                     .append("</button>");
                 $('#success > .alert-success')
-                    .append("<strong>Your message has been sent. </strong>");
+                    .append("<strong>El mensaje ha sido enviado correctamente</strong>");
                 $('#success > .alert-success')
                     .append('</div>');
 

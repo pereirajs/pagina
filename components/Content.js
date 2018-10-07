00debd7eb192299b5aa15fb7413711956d03f724
Jonathan Alvarez G <j@jonalvarezz.com>
2018-10-06 23:19:27 -0500

    Content: support ligh mode

--- components/Content.js
+++ components/Content.js
@@ -1,21 +1,22 @@
-const Content = ({ children, component: Component, ...props }) => (
+const Content = ({ children, light, component: Component, ...props }) => (
   <Component {...props}>
     {children}
 
     <style jsx>{`
       ${Component} {
-        color: var(--smoke-blue);
+        color: ${light ? "#fff" : "var(--smoke-blue)"};
       }
       ${Component} :global(h1) {
-        color: var(--dark-blue);
+        color: ${light ? "#fff" : "var(--dark-blue)"};
         margin: 0 0 1rem;
+        font-weight: bold;
       }
     `}</style>
   </Component>
 );
 
 Content.defaultProps = {
-  component: 'div'
-}
+  component: "div"
+};
 
 export default Content;

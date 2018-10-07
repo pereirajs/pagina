278fe5b4ff8fc3816be595635639ef15012c56b6
Jonathan Alvarez G <j@jonalvarezz.com>
2018-10-06 23:23:31 -0500

    Adds About Us section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Hero from "../components/hero/Hero";
 import Events from "../components/events/Events";
+import AboutUs from "../components/AboutUs";
 
 const Index = () => (
   <React.Fragment>
     <Hero />
     <Events />
+    <AboutUs />
   </React.Fragment>
 );
 
